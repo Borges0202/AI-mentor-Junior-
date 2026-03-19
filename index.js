fe2cef1d3beb30b3320c04c3df186d024866c99b
Nicolas Borges <nicolasborges736@gmail.com>
2026-03-19 14:49:20 -0300

    feat: add keyword detection

--- index.js
+++ index.js
@@ -2,17 +2,56 @@ const readline = require("readline")
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output:process.stdout
+    output: process.stdout
 })
 
-function mentorResponde(pergunta){
-    return "Boa pergunta. Antes de ver a resposta completa, tente pesar: qual conceito de programação está envolvido nisso?"
+function mentorResponde(pergunta) {
+    return "Boa pergunta. Antes de ver a resposta completa, tente pensar: qual conceito de programação está envolvido nisso?"
 }
 
-rl.question("pergunte algo sobre programação: ", (pergunta) =>{
-    const resposta = mentorResponde(pergunta)
+function respostaArray(pergunta) {
+    return "Boa pergunta sobre array"
+}
+
+function respostaFuncao(pergunta) {
+    return "Boa pergunta sobre função"
+}
+
+function respostaObjeto(pergunta) {
+    return "Boa pergunta sobre objeto"
+}
+
+function identificarPalavra(pergunta){
+
+    const limparString = pergunta.replace(/[.,;:/?!]/g, "")
+
+    const palavraChave = limparString.split(/\s+/)
+
+    const palavras = palavraChave.map(palavra => palavra.toUpperCase())
+    
+
+    if(palavras.includes("FUNCAO") || palavras.includes("FUNÇÃO")){
+        return respostaFuncao()
+    }
+    else if(palavras.includes("ARRAY")){
+        return respostaArray()
+    }
+    else if(palavras.includes("OBJETO")){
+        return respostaObjeto()
+    }
+    else{
+        return mentorResponde()
+    }
+
+}
+
+rl.question("pergunte algo sobre programação: ", (pergunta) => {
+
+
+    const resposta = identificarPalavra(pergunta)
 
     console.log(resposta)
-    
-    rl.close
+
+    rl.close()
 })
+
